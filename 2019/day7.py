5240aa5bebe4952b1da64a2ad1f7d9ccbd8bcdc5
Ryan Cox <ryancox@gmail.com>
2019-12-20 00:15:52 -0800

    WIP day 7

--- 2019/day7.py
+++ 2019/day7.py
@@ -1,6 +1,16 @@
 from processor import Processor
+from itertools import permutations
 
 with open('data/day7') as data_file:
     codes_list = [int(x) for x in data_file.read().split(',')]
+
+p = Processor(codes_list)
+
+p.process(1, 0)
+p.reset()
+p.queue_input()
+p.print()
+
+perms = permutations(range(5))
 
 
